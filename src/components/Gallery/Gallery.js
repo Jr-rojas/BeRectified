3317990e1cf11dac32ead140aa98f7940d3750fb
Jersain Rojas <j.rojas_7@hotmail.com>
2025-01-14 20:03:16 -0800

    gallery image update

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -6,7 +6,7 @@ const chunkArray = (array, chunkSize) => {
     return array.reduce((result, item, index) => {
         const chunkIndex = Math.floor(index / chunkSize);
         if (!result[chunkIndex]) {
-            result[chunkIndex] = []; // Start a new chunk
+            result[chunkIndex] = [];
         }
         result[chunkIndex].push(item);
         return result;
