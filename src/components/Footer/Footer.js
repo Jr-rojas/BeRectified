b0f51681ef06971697881c2ef9cc86b896dfc172
Jersain Rojas <j.rojas_7@hotmail.com>
2025-02-04 18:11:55 -0800

    Opening Time Update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,17 +6,18 @@ export default function Footer(){
                     <div className="col-md-5">
                         <h5>Be Rectified Barber Shop</h5>
                         <p className="mb-0">1716 Niles St, Bakersfield, CA 93305</p>
-                        <p>Phone: 661-448-0137 </p>
+                        <p>Phone: <a href="tel:661-448-0137">661-448-0137</a></p>
                         <div className="">
                             <img src=".//images/Horz_BR3_Yellow(Trans)2.png" alt="Be Rectified Barber Logo" className="img-fluid"/>
                         </div>
                     </div>
                     <div className="col-md-5">
                         <h5>Business Hours</h5>
-                        <p className="mb-0">Mon-Tues: 8 AM - 1 PM</p>
-                        <p className="mb-0">Wed: Closed</p>
+                        <p className="mb-0">Mon: Closed</p>
+                        <p className="mb-0">Tues: 9:15 AM - 1 PM</p>
+                        <p className="mb-0">Wed: 10:30 AM - 1 PM</p>
                         <p className="mb-0">Thur-Fri: 8 AM - 5 PM</p>
-                        <p className="mb-0">Sat: 8 AM - 1 PM</p>
+                        <p className="mb-0">Sat: 9 AM - 1 PM</p>
                         <p>Sun: Closed</p>
                     </div>
                 </div>
